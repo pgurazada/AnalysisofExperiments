e07e6f57a025643cf96cdefa20a8e3cacdf8cdc8
pgurazada <pavankumar.gurazada@gmail.com>
2018-03-29 07:25:08 +0400

    fixed the plot.

--- Peeking.R
+++ Peeking.R
@@ -3,12 +3,13 @@
 #' author: Pavan Gurazada
 #' output: github_document
 #' ---
-#' last update: Wed Mar 28 14:11:08 2018
+#' last update: Thu Mar 29 06:37:41 2018
 #' 
 
 library(tidyverse)
+library(ggthemes)
 
-theme_set(theme_bw())
+theme_set(theme_few())
 
 #' The setting is as follows: A website wants to check if the change in the
 #' color of their sign-up button will increase the click through rate. The
@@ -53,11 +54,10 @@ click_data_AA %>% sample_n(1000) %>%
                             avg_click = mean(current_status),
                             sd_click = sd(current_status))
 
-summary(lm(current_status ~ cond, data = click_data))
+summary(lm(current_status ~ cond, data = click_data_AA))
 
 #' The above results indicate that we cannot reject the null hypothesis that
 #' there is no difference between treatment and control
-#'
 #'
 #' Now let us assume that there is really a difference in the click-through
 #' rates for Treatment and Control.
@@ -88,30 +88,25 @@ click_data_AB %>% group_by(cond) %>%
                             sd_click = sd(current_status))
 
 #' Now if we had stopped when we reached 1000 customers
-
-click_data_AB %>% sample_n(1000) %>% 
-                  group_by(cond) %>% 
-                  summarize(n_samples = n(),
-                            avg_click = mean(current_status),
-                            sd_click = sd(current_status))
-
-#' We can run the above snippet multiple times and notice that the
+#' We can run the below snippet multiple times and notice that the
 #' variability in average clicks earned varies tremendously
 
-dev.new()
-ggplot(click_data_AB) + 
-  geom_histogram(aes(x = current_status, fill = cond), bins = 2) +
-  labs(x = "Current status",
-       y = "Count",
-       fill = "Condition",
-       title = "Results of experiment",
-       subtitle = "(Full sample - 10,000 observations)")
+click_summary_AB <- click_data_AB %>% sample_n(1000) %>% 
+                                      group_by(cond) %>% 
+                                      summarize(n_samples = n(),
+                                                avg_click = mean(current_status),
+                                                sd_click = sd(current_status))
 
-ggplot(click_data_AB %>% sample_n(1000)) + 
-  geom_histogram(aes(x = current_status, fill = cond), bins = 2) +
-  labs(x = "Current status",
-       y = "Count",
-       fill = "Condition",
-       title = "Results of experiment",
-       subtitle = "(Random sample - 1000 observations)")
-
+ggplot(click_summary_AB, aes(x = cond, y = avg_click)) +
+  geom_pointrange(aes(ymin = avg_click - sd_click, ymax = avg_click + sd_click),
+                  size = 0.8) +
+  geom_pointrange(aes(ymin = avg_click - 2 * sd_click, 
+                      ymax = avg_click + 2 * sd_click),
+                  size = 0.3) +
+  geom_hline(aes(yintercept = 0.02), linetype = "dashed") +
+  scale_x_discrete("Condition", 
+                   labels = c("Control", "Treatment")) +
+  scale_y_continuous("", breaks = c(c(0.02, 0.1, 0.2, 0.3))) +
+  coord_flip() +
+  ggtitle("Average click through percentage by condition", 
+          subtitle = "(random sample of 1000 customers)")
